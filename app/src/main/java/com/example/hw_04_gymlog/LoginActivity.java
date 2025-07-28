b7fccc6e491b1c53df86b3908031f20d9222ed17
Claudia <cfierro@csumb.edu>
2025-07-28 12:43:09 -0700

    End of Video : Created login screen

---
+++ app/src/main/java/com/example/hw_04_gymlog/LoginActivity.java
@@ -0,0 +1,39 @@
+package com.example.hw_04_gymlog;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.hw_04_gymlog.databinding.ActivityLoginBinding;
+
+public class LoginActivity extends AppCompatActivity {
+    private ActivityLoginBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityLoginBinding.inflate(getLayoutInflater());
+        setContentView(R.layout.activity_login);
+
+        binding.loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                Intent intent = MainActivity.mainActivityIntentFactory(getApplicationContext(), 0);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+    static Intent loginIntetntFactory(Context context){
+        return new Intent(context, LoginActivity.class);
+
+    }
+}
